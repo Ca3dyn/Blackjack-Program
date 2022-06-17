1647c9269b084018dfec69009e7be65145e135ac
Caedyn Swaggerman <cloigmanmiller@gmail.com>
2022-06-17 12:49:39 -0700

    modified:   .gitignore 	modified:   Program.cs

--- Program.cs
+++ Program.cs
@@ -6,6 +6,24 @@ namespace Blackjack
     class Program
     {
 
+        static String[,] cardDeck = { {"Ace of Spades", "1 or 11" }, {"Two of Spades", "2"}, {"Three of Spades", "3"}, 
+            {"Four of Spades", "4"}, {"Five of Spades", "5"}, {"Six of Spades", "6"}, {"Seven of Spades", "7"},
+            {"Eight of Spades", "8"}, {"Nine of Spades", "9"}, {"Ten of Spades", "10"}, {"Jack of Spades", "10"},
+            {"Queen of Spades", "10"}, {"King of Spades", "10"}, {"Ace of Clubs", "1 or 11"}, {"Two of Clubs", "20"},
+            {"Three of Clubs", "3"}, {"Four of Clubs", "4"}, {"Five of Clubs", "5"}, {"Six of Clubs", "6"}, 
+            {"Seven of Clubs", "7"}, {"Eight of Clubs", "8"}, {"Nine of Clubs", "9"}, {"Ten of Clubs", "10"}, 
+            {"Jack of Clubs", "10"}, {"Queen of Clubs", "10"}, {"King of Clubs", "10"}, {"Ace of Hearts", "1 or 11"},
+            {"Two of Hearts", "2"}, {"Three of Hearts", "3"}, {"Four of Hearts", "4"}, {"Five of Hearts", "5"}, 
+            {"Six of Hearts", "6"}, {"Seven of Hearts", "7"}, {"Eight of Hearts", "8"}, {"Nine of Hearts", "9"}, 
+            {"Ten of Hearts", "10"}, {"Jack of Hearts", "10"}, {"Queen of Hearts", "10"}, {"King of Hearts", "10"}, 
+            {"Ace of Diamonds", "1 or 11"}, {"Two of Diamonds", "2"}, {"Three of Diamonds", "3"}, {"Four of Diamonds", "4"}, 
+            {"Five of Diamonds", "5"}, {"Six of Diamonds", "6"}, {"Seven of Diamonds", "7"}, {"Eight of Diamonds", "8"}, 
+            {"Nine of Diamonds", "9"}, {"Ten of Diamonds", "10"}, {"Jack of Diamonds", "10"}, 
+            {"Queen of Diamonds", "10"}, {"King of Diamonds", "10"} };
+            
+            static List<int> randomNums = new List<int>();
+            static List<string> norepeatCards = new List<string>();
+
         public static void Main(string[] args){
             
             bool z = true;
@@ -15,9 +33,10 @@ namespace Blackjack
             
                 Console.WriteLine("Welcome to Blackjack.");
 
-                Choices("The cards are being dealt. Take two cards?\n(y or n)\nInsert c to see list of all generated cards.", "You take two cards", "Why did you not take the cards?\nTry again, you utterly failed the game. I'm disappointed in you.", "Here are all the cards:\n");
+                Choices("The cards are being dealt. Take two cards?\n(y or n)\nInsert c to see list of all generated cards.", "You take two cards", "Why did you not take the cards?\nTry again, you utterly failed the game. I'm disappointed in you.", "Here are all the cards:");
 
                 Console.ReadKey();
+                randomNums.Clear();
                 Console.Clear();
             
             }
@@ -28,8 +47,8 @@ namespace Blackjack
 
         public static void Choices(string question, string optionOne, string optionTwo, string optionThree) {
 
-            char userOption;
             Console.WriteLine(question);
+            char userOption;
 
             userOption = Console.ReadKey().KeyChar;
 
@@ -37,6 +56,15 @@ namespace Blackjack
 
                 Console.Clear();    
                 Console.WriteLine(optionOne);
+
+                int z = norepeatCards.Count;
+                Console.WriteLine(z);
+
+                for(int i = 0; i < norepeatCards.Count; i++) {
+                
+                Console.WriteLine(norepeatCards[i]);
+
+                }
         
             }
 
@@ -51,8 +79,9 @@ namespace Blackjack
                 
                 Console.Clear();
                 Console.WriteLine(optionThree);
+
                 Cards();
-                    
+                        
             }
 
             else if (userOption == '`') {
@@ -71,64 +100,35 @@ namespace Blackjack
         
         } 
 
-        public static string Cards() {
-
-            String[,] cardDeck = { {"Ace of Spades", "1 or 11" }, {"Two of Spades", "2"}, {"Three of Spades", "3"}, 
-            {"Four of Spades", "4"}, {"Five of Spades", "5"}, {"Six of Spades", "6"}, {"Seven of Spades", "7"},
-            {"Eight of Spades", "8"}, {"Nine of Spades", "9"}, {"Ten of Spades", "10"}, {"Jack of Spades", "10"},
-            {"Queen of Spades", "10"}, {"King of Spades", "10"}, {"Ace of Clubs", "1 or 11"}, {"Two of Clubs", "20"},
-            {"Three of Clubs", "3"}, {"Four of Clubs", "4"}, {"Five of Clubs", "5"}, {"Six of Clubs", "6"}, 
-            {"Seven of Clubs", "7"}, {"Eight of Clubs", "8"}, {"Nine of Clubs", "9"}, {"Ten of Clubs", "10"}, 
-            {"Jack of Clubs", "10"}, {"Queen of Clubs", "10"}, {"King of Clubs", "10"}, {"Ace of Hearts", "1 or 11"},
-            {"Two of Hearts", "2"}, {"Three of Hearts", "3"}, {"Four of Hearts", "4"}, {"Five of Hearts", "5"}, 
-            {"Six of Hearts", "6"}, {"Seven of Hearts", "7"}, {"Eight of Hearts", "8"}, {"Nine of Hearts", "9"}, 
-            {"Ten of Hearts", "10"}, {"Jack of Hearts", "10"}, {"Queen of Hearts", "10"}, {"King of Hearts", "10"}, 
-            {"Ace of Diamonds", "1 or 11"}, {"Two of Diamonds", "2"}, {"Three of Diamonds", "3"}, {"Four of Diamonds", "4"}, 
-            {"Five of Diamonds", "5"}, {"Six of Diamonds", "6"}, {"Seven of Diamonds", "7"}, {"Eight of Diamonds", "8"}, 
-            {"Nine of Diamonds", "9"}, {"Ten of Diamonds", "10"}, {"Jack of Diamonds", "10"}, 
-            {"Queen of Diamonds", "10"}, {"King of Diamonds", "10"} };
-
+        public static void Cards() {
 
             Random numGen = new Random();
-            int h = 0;
             int i = 0;
-           
-            HashSet<int> repeatedNums = new HashSet<int>();
 
             int testNumber = numGen.Next(0, 52);
             string everything = (cardDeck [testNumber, 0] + " = " + cardDeck [testNumber, 1]);
+    
+            while (i <= 51) {
 
-            while (i <= 52) {
+                testNumber = numGen.Next(0, 52);
+                everything = (cardDeck [testNumber, 0 ] + " = " + cardDeck [testNumber, 1]);
 
-            testNumber = numGen.Next(0, 52);
-            everything = (cardDeck [testNumber, 0] + " = " + cardDeck [testNumber, 1]);
-
-                if (!repeatedNums.Contains(testNumber)) {
+                    if (!randomNums.Contains(testNumber)) {
+                        
+                        randomNums.Add(testNumber);
+                        norepeatCards.Add(everything);
+                        Console.WriteLine(everything);
+                        i++;
                     
-                    repeatedNums.Add(testNumber);
-                    Console.WriteLine(everything);
-                    i++;
-                    h++;
-                
-                }
-
-                else if(i == 52) {
-
-                    break;
-                    
-                }
+                    }
 
             }
-
-            return everything;
                 
         }
 
-        string universe =  Cards().everything;
-
         public static string playerHand() {
           
-            if (userOption == 'y') {}
+            //if (userOption == 'y') {}
 
             return "something"; 
            

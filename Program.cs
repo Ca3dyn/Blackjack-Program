b07d276d33b1a975622202369da899e33d1fba60
Caedyn Swaggerman <cloigmanmiller@gmail.com>
2022-06-19 17:28:21 -0700

    modified:   Program.cs

--- Program.cs
+++ Program.cs
@@ -33,6 +33,7 @@ namespace Blackjack
             
                 Console.WriteLine("Welcome to Blackjack.");
 
+                Cards();
                 Choices("The cards are being dealt. Take two cards?\n(y or n)\nInsert c to see list of all generated cards.", "You take two cards", "Why did you not take the cards?\nTry again, you utterly failed the game. I'm disappointed in you.", "Here are all the cards:");
 
                 Console.ReadKey();
@@ -56,16 +57,8 @@ namespace Blackjack
 
                 Console.Clear();    
                 Console.WriteLine(optionOne);
+                dealingCards();
 
-                int z = norepeatCards.Count;
-                Console.WriteLine(z);
-
-                for(int i = 0; i < norepeatCards.Count; i++) {
-                
-                Console.WriteLine(norepeatCards[i]);
-
-                }
-        
             }
 
             else if (userOption == 'n') {
@@ -79,8 +72,15 @@ namespace Blackjack
                 
                 Console.Clear();
                 Console.WriteLine(optionThree);
+                int z = norepeatCards.Count;
 
-                Cards();
+                    for(int i = 0; i < 52; i++) {
+                    
+                        Console.WriteLine(norepeatCards[i]);
+
+                    }
+
+                Console.WriteLine("There are " + z + " cards.");
                         
             }
 
@@ -94,8 +94,8 @@ namespace Blackjack
             else if (userOption != 'y' || userOption != 'n' || userOption != 'c' || userOption != '`')  {
             
                 Console.Clear();
-                Console.WriteLine("Please input a valid character.\nPress any key to return to the start.");
-
+                Console.WriteLine("Please input a valid character.");
+                Console.WriteLine("Press any key to return to the start.");
             } 
         
         } 
@@ -104,20 +104,18 @@ namespace Blackjack
 
             Random numGen = new Random();
             int i = 0;
-
-            int testNumber = numGen.Next(0, 52);
-            string everything = (cardDeck [testNumber, 0] + " = " + cardDeck [testNumber, 1]);
+            int randomNumber = numGen.Next(0, 52);
+            string everything = (cardDeck [randomNumber, 0] + " = " + cardDeck [randomNumber, 1]);
     
             while (i <= 51) {
 
-                testNumber = numGen.Next(0, 52);
-                everything = (cardDeck [testNumber, 0 ] + " = " + cardDeck [testNumber, 1]);
+                randomNumber = numGen.Next(0, 52);
+                everything = (cardDeck [randomNumber, 0 ] + " = " + cardDeck [randomNumber, 1]);
 
-                    if (!randomNums.Contains(testNumber)) {
+                    if (!randomNums.Contains(randomNumber)) {
                         
-                        randomNums.Add(testNumber);
+                        randomNums.Add(randomNumber);
                         norepeatCards.Add(everything);
-                        Console.WriteLine(everything);
                         i++;
                     
                     }
@@ -126,22 +124,31 @@ namespace Blackjack
                 
         }
 
-        public static string playerHand() {
+        public static void dealingCards() {
           
-            //if (userOption == 'y') {}
+            Random cardGen = new Random();
+            int randomCard = cardGen.Next(0,52);
+            // Put this somewhere later: Console.WriteLine(norepeatCards[randomCard]);
 
-            return "something"; 
+            for (int i = 0; i < 52; i++) {
+
+
+                
+            }
+            
+            if (randomNums.Contains(randomCard)) {
+
+                Console.WriteLine("The index from norepeatCards: " + norepeatCards[randomCard]);
+                Console.WriteLine("The index from randomNums: " + randomNums[randomCard]);
+
+            //make it so if the list contains the item give it to the player via maybe
+            //a list and then remove it from the original list
+            //if no repeat num list contains a number, save the number then print out that 
+            //id number of the carddeck list and remove that index number from both lists
+
+            }
            
         }
-
-        public static string dealerHand() {
-
-
-
-            return "something";
-
-        }
-
         
     }
 }

61695ffc74f53abda95c3f95980fd028a7305a07
Caedyn Swaggerman <cloigmanmiller@gmail.com>
2022-06-30 23:04:24 -0700

    deleted:    .gitignore.txt 	modified:   Program.cs 	modified:   bin/Debug/net6.0/Blackjack v2.0.dll 	modified:   bin/Debug/net6.0/Blackjack v2.0.pdb 	modified:   obj/Debug/net6.0/Blackjack v2.0.dll 	modified:   obj/Debug/net6.0/Blackjack v2.0.pdb

--- Program.cs
+++ Program.cs
@@ -7,17 +7,17 @@ namespace Blackjack
     class Program
     {
 
-        static String[,] cardDeck = { {"Ace of Spades", "1 or 11" }, {"Two of Spades", "2"}, {"Three of Spades", "3"}, 
+        static String[,] cardDeck = { {"Ace of Spades", "11" }, {"Two of Spades", "2"}, {"Three of Spades", "3"}, 
             {"Four of Spades", "4"}, {"Five of Spades", "5"}, {"Six of Spades", "6"}, {"Seven of Spades", "7"},
             {"Eight of Spades", "8"}, {"Nine of Spades", "9"}, {"Ten of Spades", "10"}, {"Jack of Spades", "10"},
-            {"Queen of Spades", "10"}, {"King of Spades", "10"}, {"Ace of Clubs", "1 or 11"}, {"Two of Clubs", "2"},
+            {"Queen of Spades", "10"}, {"King of Spades", "10"}, {"Ace of Clubs", "11"}, {"Two of Clubs", "2"},
             {"Three of Clubs", "3"}, {"Four of Clubs", "4"}, {"Five of Clubs", "5"}, {"Six of Clubs", "6"}, 
             {"Seven of Clubs", "7"}, {"Eight of Clubs", "8"}, {"Nine of Clubs", "9"}, {"Ten of Clubs", "10"}, 
-            {"Jack of Clubs", "10"}, {"Queen of Clubs", "10"}, {"King of Clubs", "10"}, {"Ace of Hearts", "1 or 11"},
+            {"Jack of Clubs", "10"}, {"Queen of Clubs", "10"}, {"King of Clubs", "10"}, {"Ace of Hearts", "11"},
             {"Two of Hearts", "2"}, {"Three of Hearts", "3"}, {"Four of Hearts", "4"}, {"Five of Hearts", "5"}, 
             {"Six of Hearts", "6"}, {"Seven of Hearts", "7"}, {"Eight of Hearts", "8"}, {"Nine of Hearts", "9"}, 
             {"Ten of Hearts", "10"}, {"Jack of Hearts", "10"}, {"Queen of Hearts", "10"}, {"King of Hearts", "10"}, 
-            {"Ace of Diamonds", "1 or 11"}, {"Two of Diamonds", "2"}, {"Three of Diamonds", "3"}, {"Four of Diamonds", "4"}, 
+            {"Ace of Diamonds", "11"}, {"Two of Diamonds", "2"}, {"Three of Diamonds", "3"}, {"Four of Diamonds", "4"}, 
             {"Five of Diamonds", "5"}, {"Six of Diamonds", "6"}, {"Seven of Diamonds", "7"}, {"Eight of Diamonds", "8"}, 
             {"Nine of Diamonds", "9"}, {"Ten of Diamonds", "10"}, {"Jack of Diamonds", "10"}, 
             {"Queen of Diamonds", "10"}, {"King of Diamonds", "10"} };
@@ -32,6 +32,9 @@ namespace Blackjack
             bool b = true;
             Console.Title = "Caedyn's Blackjack Program v3.0";
 
+            //I need to make it so aces are worth 11 at first but if the totalValue of the player's
+            //cards goes over 21 and an ace is in their hand then make it automatically -10 or something
+
             do {
             
                 Console.WriteLine("Welcome to Blackjack.");
@@ -43,6 +46,7 @@ namespace Blackjack
                 randomNums.Clear();
                 playerCards.Clear();
                 norepeatCards.Clear();
+                randomnumOrder.Clear();
                 Console.Clear();
             
             }
@@ -88,18 +92,10 @@ namespace Blackjack
                     for(int i = 0; i < 52; i++) {
                     
                         Console.WriteLine(norepeatCards[i]);
-                        Console.WriteLine(randomnumOrder[i]);
 
                     }
 
                 Console.WriteLine("\nThere are " + z + " cards.");
-                        
-            }
-
-            else if (userOption == 'e') {
-                
-                Console.Clear();
-                Console.WriteLine("Mr. Eien Kobayashi in the flesh. Or I guess not flesh.\nHe's here though I guess.");
                         
             }
 
@@ -129,11 +125,12 @@ namespace Blackjack
                     
             }
 
-            else if (userOption != 'y' || userOption != 'n' || userOption != 'c' || userOption != 'e' || userOption != '`')  {
+            else if (userOption != 'y' || userOption != 'n' || userOption != 'c' || userOption != '`')  {
             
                 Console.Clear();
                 Console.WriteLine("Please input a valid character.");
                 Console.WriteLine("Press any key to return to the start.");
+
             } 
         
         }
@@ -165,22 +162,19 @@ namespace Blackjack
           
             Random cardGen = new Random();
             int randomCard = cardGen.Next(0,52);
+            int totalValue = new int();
             int i = 0;
-
+            
+            
             while (i < 2) {
 
                 randomCard = cardGen.Next(0, 52);
 
-                if (i == 2) {
-
-                    Console.WriteLine("'i' is equal to 2.");
-
-                    for(int h = 0; h < 52; h++) {
+                for(int h = 0; h < 52; h++) {
                     
-                        //string total = toint32.cardDeck[randomnumOrder[h] , 1];
-                        //Find a way to print out the values of cards and make it
-                        //into an int which can be added together and combined into one
-                    }
+                    //string total = toint32.cardDeck[randomnumOrder[h] , 1];
+                    //Find a way to print out the values of cards and make it
+                    //into an int which can be added together and combined into one
 
                 }
 
@@ -188,17 +182,20 @@ namespace Blackjack
 
                     Console.WriteLine(cardDeck[randomnumOrder[i] , 0] + " = " + cardDeck[randomnumOrder[i] , 1]);
                     playerCards.Add(cardDeck[randomnumOrder[i] , 0] + " = " + cardDeck[randomnumOrder[i] , 1]);
-                    randomNums.Remove(randomCard);    
+                    randomNums.Remove(randomCard); 
+                    int value = Convert.ToInt32(cardDeck[randomnumOrder[i] , 1]);
+                    totalValue += value;
                     i++;          
 
                 }
 
             }
 
+            Console.WriteLine(totalValue);
             bool z = true;
             char userOption;
 
-            do {
+            while (i < 52) {
 
                 userOption = Console.ReadKey().KeyChar;
 
@@ -210,34 +207,44 @@ namespace Blackjack
 
                         Console.Clear();
                         playerCards.Add(cardDeck[randomnumOrder[i] , 0] + " = " + cardDeck[randomnumOrder[i] , 1]);
-                        randomNums.Remove(randomCard);           
+                        randomNums.Remove(randomCard);
+                        int value = Convert.ToInt32(cardDeck[randomnumOrder[i] , 1]);
+                        totalValue += value;     
+                        i++;      
                         
                         for (int t = 0; t < playerCards.Count; t++) {
-
+                        
                             Console.WriteLine(cardDeck[randomnumOrder[t] , 0] + " = " + cardDeck[randomnumOrder[t] , 1]);
                          
                         }
+
+                        Console.WriteLine(totalValue);
 
                     }
 
                 }
 
                 else if (userOption == 'n') {
+                    
+                    Console.Clear();
+                    
+                    for (int t = 0; t < playerCards.Count; t++) {
+                        
+                        Console.WriteLine(cardDeck[randomnumOrder[t] , 0] + " = " + cardDeck[randomnumOrder[t] , 1]);
+                         
+                    }
 
+                    Console.WriteLine("This means that you don't want any more cards.");
+                    Console.WriteLine("You ended with " + totalValue + " points.");
                     z = false;
+                    break;
+                    
 
                 }
 
             }
             
             while (z == true);
-
-            //Somehow find a way to make a variable which adds finds the total amount of
-            //index rows of playerCards list and add up the values of those indexes
-
-            //You can possibly make two different lists for the player's cards. 
-            //One to store the name
-            //and one to store the value
 
         }
 
